eab709c4fcf183ee26119f1254cf60305c07cdba
Daniel Pogue <shadowfelldown@gmail.com>
2016-06-12 00:02:41 -0400

    adding a sketch to test the touchsense with potentiometer.

--- Design/Testing/TouchSense_Potentiometer/TouchSense_Potentiometer.ino
+++ Design/Testing/TouchSense_Potentiometer/TouchSense_Potentiometer.ino
@@ -1,8 +1,10 @@
 const int pins = 2;
 int touchPin[pins] = {0, 1};
 boolean touched = false;
-int potPin = A0;
+//int potPin = A0;
+int ledPin = 13; //LED conectado ao pino digital 13
 int sensitivity = 400;
+int spark = 2;
 int read1[pins], base[pins];
 int column, row, c;
 void setup() {
@@ -10,14 +12,18 @@ void setup() {
    for (int c=0; c<pins; c++) {
      base[c]=touchRead(touchPin[c]);// Baseline calibration
       Serial.println(base[c]);
+pinMode (ledPin, OUTPUT);
+pinMode (spark, OUTPUT);
    }
 }
 
 void loop() {
- sensitivity =  map(analogRead(potPin), 1, 1023, 300, 1000);
+// sensitivity =  map(analogRead(potPin), 1, 1023, 300, 1000);
  //Serial.println(sensitivity);
  //Serial.println(analogRead(potPin));
   // put your main code here, to run repeatedly:
+digitalWrite(spark, LOW);
+digitalWrite (ledPin, LOW);
 touched = false;
   for (c=0; c<pins; c++) {
      read1[c] = touchRead(touchPin[c]);
@@ -41,6 +47,9 @@ touched = false;
          Serial.print(" Threshold: "); // print a space
          Serial.print(sensitivity);
          Serial.println();
+       digitalWrite(spark, HIGH);
+       digitalWrite (ledPin, HIGH);
+       delay(400);
        }
   }
 if (touched) {
