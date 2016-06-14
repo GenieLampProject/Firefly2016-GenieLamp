7f4a3958aa545387557f9cb68dd7324f26c71d97
James Fraumeni <jfraumeni@hotmail.com>
2016-06-14 00:53:05 -0400

    =Added initial input/output modules' interfaces Now compiling successfully

--- genie_lamp/genie_lamp.ino
+++ genie_lamp/genie_lamp.ino
@@ -13,44 +13,103 @@
 /*** FINISH Config ***/
 
 
+// XXX TODO XXX Add to the appropriate specific files
 /*** BEGIN Modules Interfaces***/
+  /** BEGIN Modules Declarations ***/
 class ModuleBase
 {
     public:
-        void setup();
-        void initialize();
+        virtual void setup() = 0;
+        virtual void initialize() = 0;
 };
 
 class InputBase : public ModuleBase
 {
     public:
-        int touched_time();
-    private:
-        int _touched_millis = 0;
+        virtual int touched_time() = 0;
 };
 
 class Touch : public InputBase
 {
-    int DELETEME;
+    public:
+        virtual void setup();
+        virtual void initialize();
+        virtual int touched_time();
+    private:
+        int _touched_millis = 0;
 };
 
 class OutputBase : public ModuleBase
 {
     public:
-        void off();
-        void display(int);
+        virtual void off() = 0;
+        virtual void display(int millis) = 0;
 };
 
 class Poofer : public OutputBase
 {
-    int DELETEME;
+    public:
+        virtual void setup();
+        virtual void initialize();
+        virtual void off();
+        virtual void display(int millis);
 };
 
 class BodyLEDs : public OutputBase
 {
-    int DELETEME;
+    public:
+        virtual void setup();
+        virtual void initialize();
+        virtual void off();
+        virtual void display(int millis);
 };
-// XXX TODO XXX Add to the appropriate specific files
+  /** FINISH Modules Declarations ***/
+    
+  /** BEGIN Modules Definitions ***/
+    /** BEGIN Touch Modules Definitions ***/
+    void Touch::setup() {
+        0;      // XXX TODO XXX
+    }
+    void Touch::initialize() {
+        0;      // XXX TODO XXX
+    }
+    int Touch::touched_time() {
+        return 0;      // XXX TODO XXX
+    }
+    /** FINISH Touch Modules Definitions ***/
+
+    /** BEGIN Poofer Modules Definitions ***/
+    void Poofer::setup() {
+        0;      // XXX TODO XXX
+    }
+    void Poofer::initialize() {
+        0;      // XXX TODO XXX
+    }
+    void Poofer::off() {
+        0;      // XXX TODO XXX
+    }
+    void Poofer::display(int millis) {
+        0;      // XXX TODO XXX
+    }
+    /** FINISH Poofer Modules Definitions ***/
+
+    /** BEGIN BodyLEDs Modules Definitions ***/
+    void BodyLEDs::setup() {
+        0;      // XXX TODO XXX
+    }
+    void BodyLEDs::initialize() {
+        0;      // XXX TODO XXX
+    }
+    void BodyLEDs::off() {
+        0;      // XXX TODO XXX
+    }
+    void BodyLEDs::display(int millis) {
+        0;      // XXX TODO XXX
+    }
+    /** FINISH BodyLEDs Modules Definitions ***/
+    /** FINISH Touch Modules Definitions ***/
+
+  /** FINISH Modules Definitions ***/
 /*** FINISH Modules Interfaces***/
 
 
