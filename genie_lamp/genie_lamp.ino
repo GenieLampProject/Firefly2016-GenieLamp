52a3ec4a068859d2190600411aafaeb244a50c07
James Fraumeni <jfraumeni@hotmail.com>
2016-06-14 00:33:19 -0400

    =Fleshed out modules interfaces

--- genie_lamp/genie_lamp.ino
+++ genie_lamp/genie_lamp.ino
@@ -13,13 +13,67 @@
 /*** FINISH Config ***/
 
 
+/*** BEGIN Modules Interfaces***/
+class ModuleBase
+{
+    public:
+        void setup();
+        void initialize();
+};
+
+class InputBase : public ModuleBase
+{
+    public:
+        int touched_time();
+    private:
+        int _touched_millis = 0;
+};
+
+class Touch : public InputBase
+{
+    int DELETEME;
+};
+
+class OutputBase : public ModuleBase
+{
+    public:
+        void off();
+        void display(int);
+};
+
+class Poofer : public OutputBase
+{
+    int DELETEME;
+};
+
+class BodyLEDs : public OutputBase
+{
+    int DELETEME;
+};
+// XXX TODO XXX Add to the appropriate specific files
+/*** FINISH Modules Interfaces***/
+
+
+
+
+/*** BEGIN Control Initialization***/
+Touch* touch;
+Poofer* poofer;
+BodyLEDs* bodyLEDs;
+/*** FINISH Control Initialization***/
+
+
 
 
 /*** BEGIN Setup Routine ***/
 void setup() {
-    Touch.setup();
-    Poofer.setup();
-    BodyLEDs.setup();
+    touch = new Touch();
+    poofer = new Poofer();
+    bodyLEDs = new BodyLEDs();
+
+    touch->setup();
+    poofer->setup();
+    bodyLEDs->setup();
 }
 /*** FINISH Setup Routine ***/
 
@@ -29,87 +83,25 @@ void setup() {
 /*** BEGIN Main operation entry point ***/
 void loop() {
     // Initialize
-    Touch.initialize();
-    Poofer.initialize();
-    BodyLEDs.initialize();
+    touch->initialize();
+    poofer->initialize();
+    bodyLEDs->initialize();
     
 
     // Main operational loop
     int touched_millis;
     while (true) {
-        touched_millis = Touch.touched_time();
+        touched_millis = touch->touched_time();
         if (!touched_millis) {
-            Poofer.off();
-            BodyLEDs.off();
+            poofer->off();
+            bodyLEDs->off();
         } else {
             // XXX TODO XXX See if we should progress to the next output stage
-            Poofer.display(!touched_millis) {
-            BodyLEDs.display();
+            poofer->display(touched_millis);
+            bodyLEDs->display(touched_millis);
         }
     }
 
 }
 /*** FINISH Main operation entry point ***/
 
-
-/*
-const int pins = 2;
-int touchPin[pins] = {0, 1};
-boolean touched = false;
-//int potPin = A0;
-int ledPin = 13; //LED conectado ao pino digital 13
-int sensitivity = 400;
-int spark = 2;
-int read1[pins], base[pins];
-int column, row, c;
-void setup() {
-   Serial.begin(38400);
-   for (int c=0; c<pins; c++) {
-     base[c]=touchRead(touchPin[c]);// Baseline calibration
-      Serial.println(base[c]);
-pinMode (ledPin, OUTPUT);
-pinMode (spark, OUTPUT);
-   }
-}
-
-void loop() {
-// sensitivity =  map(analogRead(potPin), 1, 1023, 300, 1000);
- //Serial.println(sensitivity);
- //Serial.println(analogRead(potPin));
-  // put your main code here, to run repeatedly:
-digitalWrite(spark, LOW);
-digitalWrite (ledPin, LOW);
-touched = false;
-  for (c=0; c<pins; c++) {
-     read1[c] = touchRead(touchPin[c]);
-     //Serial.println(read1[c]-base[c]);
-     int Difference = read1[c]-base[c];
-       if (read1[c]-base[c] > sensitivity) {
-         touched = true;
-         String sensorNum = c;
-        // Disabled for Open Studios
-        //String output = " Sensor " + sensorNum;
-        // Serial.println(output);
-        // Serial.println(Difference);
-         Serial.print(" Sensor: ");
-         Serial.print(sensorNum);
-         Serial.print(" Base: "); // print a space
-         Serial.print(base[c]);
-         Serial.print("  Read: "); // print a space
-         Serial.print(read1[c]);
-         Serial.print(" BL diff: "); // print a space
-         Serial.print(Difference);
-         Serial.print(" Threshold: "); // print a space
-         Serial.print(sensitivity);
-         Serial.println();
-       digitalWrite(spark, HIGH);
-       digitalWrite (ledPin, HIGH);
-       delay(400);
-       }
-  }
-if (touched) {
-
-   // Serial.println(read1[c]-base[c]);
-}
-}
-*/
