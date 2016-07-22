caf713b3ff1b1ae083500cf700421e6456d6409f
shadowfelldown <shadowfelldown@gmail.com>
2016-07-22 03:53:52 -0400

    = Updating the DMX test for smoke+leds+communication can now trigger smoke machine off of serial calls. TODO: need to figure out why test of the two configuration failed.

--- Design/Testing/dmxtest/dmxtest.ino
+++ Design/Testing/dmxtest/dmxtest.ino
@@ -1,18 +1,43 @@
-/* Welcome to DmxMaster. This library allows you to control DMX stage and
-** architectural lighting and visual effects easily from Arduino. DmxMaster
-** is compatible with the Tinker.it! DMX shield and all known DIY Arduino
-** DMX control circuits.
-**
-** DmxMaster is available from: http://code.google.com/p/tinkerit/
-** Help and support: http://groups.google.com/group/DmxMaster */
+/*
+  Serial Call and Response
+ Language: Wiring/Arduino
 
-/* To use DmxMaster, you will need the following line. Arduino will
-** auto-insert it if you select Sketch > Import Library > DmxMaster. */
+ This program sends an ASCII A (byte of value 65) on startup
+ and repeats that until it gets some data in.
+ Then it waits for a byte in the serial port, and
+ sends three sensor values whenever it gets a byte in.
 
+ Thanks to Greg Shakar and Scott Fitzgerald for the improvements
+
+   The circuit:
+ * potentiometers attached to analog inputs 0 and 1
+ * pushbutton attached to digital I/O 2
+
+ Created 26 Sept. 2005
+ by Tom Igoe
+ modified 24 April 2012
+ by Tom Igoe and Scott Fitzgerald
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/SerialCallResponse
+
+ */
 #include <DmxMaster.h>
-
+int firstSensor = 0;    // first analog sensor
+int secondSensor = 0;   // second analog sensor
+int thirdSensor = 0;    // digital sensor
+int inByte;         // incoming serial byte
+/**Light Stuff **/
+int red=0;
+int green=0;
+int blue=0;
+/**Smoke Stuff **/
+int smoke = 0;
 void setup() {
-  /* The most common pin for DMX output is pin 3, which DmxMaster
+
+ /**DMX MASTER CODE**/
+   /* The most common pin for DMX output is pin 3, which DmxMaster
 ** uses by default. If you need to change that, do it here. */
   DmxMaster.usePin(3);
 
@@ -22,20 +47,60 @@ void setup() {
 ** do this, DmxMaster will set the maximum channel number to the
 ** highest channel you DmxMaster.write() to. */
   DmxMaster.maxChannel(512);
+ /**COMMUNICATIONS CODE**/
+  // start serial port at 9600 bps:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  establishContact();  // send a byte to establish contact until receiver responds
 }
 
 void loop() {
-  int brightness;
-  /* Simple loop to ramp up brightness */
-  for (brightness = 0; brightness <= 255; brightness++) {
-    
-    /* Update DMX channel 1 to new brightness */
-    DmxMaster.write(1, 122);
-    
-    /* Small delay to slow down the ramping */
-    delay(brightness);
+  // if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    int smokeIn = Serial.parseInt();
+    int redIn = Serial.parseInt();
+    int greenIn = Serial.parseInt();
+    int blueIn = Serial.parseInt();
+     if (Serial.read() == '\n') {
+    if (redIn && blueIn && greenIn == 0){
+      red= red--;
+      blue = blue--;
+      green = green--;
+      smoke = smokeIn;
+    }
+    else{
+      red = redIn;
+      blue = blueIn;
+      green = greenIn;
+      smoke = smokeIn;
+    }
+    DmxMaster.write(2, red);
+    Serial.print("Red Brightness: ");
+    Serial.println(red);
+    DmxMaster.write(3, blue);
+    Serial.print("Blue Brightness: ");
+    Serial.println(blue);
+    DmxMaster.write(4, green);
+    Serial.print("Green Brightness: ");
+    Serial.println(green);
+    if (smoke != 0){
+    DmxMaster.write(1, 255);
+    Serial.println("machine On poofing");
+    delay(smoke*1000);
+    DmxMaster.write(1, 0);
+    Serial.println("machine stopped poofing");
+    }
   }
-
+  }
 }
 
-
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');   // send a capital A
+    delay(300);
+  }
+}
